8c289e575b3b5ac91f519fb70c74e66d742f7487
Mitch Kelly <mitchh.kelly@gmail.com>
2014-07-09 12:20:39 +1000

    Add features and tidy comments
    -Add feature to check if the login was successful by parsing telnet
    output for a valid privileged-exec prompt (Cisco)
    -Add working hosts.csv file
    -Add report at the end of the script so that the user can see hosts that
    were not reachable and hosts that were reachable but had an invalid
    login.
    -Comment the entire script so that it can be easily understood.
    -Add some TODOs in the comments that will need to be fixed and worked on
    later.

--- Powershell/remoteCommand.ps1
+++ Powershell/remoteCommand.ps1
@@ -27,17 +27,17 @@ $filepath ="C:\Audit\Configs\" ## location to store the output files
 #////////////////// Connection Settings ////////////////////////
 
 $connectionType ="telnet" ## connection type can be 'telnet' or 'ssh'
-$username ="" ## username for device logins
-$password ="" ## password for device logins
+$username ="cisco" ## username for device logins
+$password ="cisco" ## password for device logins
 $termlength = "term len 0" ## useful for older consoles that have line display limitations
 $enable = "en" ## useful for appliances like cisco switches that have an 'enable' command mode
 $enablepassword = "" ## add enable password if there is one
 $commandDelay = 1000 ## add a delay between commands
 
 #////////////////// Command Settings ////////////////////////
-$runCommands = ("show run","show ip int brie")
+$runCommands = ("show tech")
 
-
+$pattern = '([A-Z])\w+#'
 
 ################################################################
 ##################### Optional Section #########################
@@ -73,138 +73,203 @@ $p1 =""
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # DON'T ALTER THE CODE BELOW
+# NO USER TUNABLE SETTINGS
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 ## create array to store failed connections
-$hostsWithError = @()
+$deadHosts = @()
+$hostNoLogin = @()
+
 
 ## read output from remote host
 function GetOutput
 {
-  ## buffer to receive the response
-  $buffer = new-object System.Byte[] 1024
-  $encoding = new-object System.Text.AsciiEncoding
+    ## buffer to receive the response
+    $buffer = new-object System.Byte[] 1024 ## buffer size
+    $encoding = new-object System.Text.AsciiEncoding ## buffer encoding
 
-  $outputBuffer = ""
-  $foundMore = $false
+    $outputBuffer = "" ## define and clear the output from this function
+    $foundMore = $false ## needs to be false to first check the buffer
 
-  ## read all data available from the stream, writing it to the
-  ## output buffer when done.
-  do
-  {
-    ## allow data to buffer for a bit
-    start-sleep -m 1000
-
-    ## read what data is available
-    $foundmore = $false
-    $stream.ReadTimeout = 1000
-
+    ## read all data available from the stream, writing it to the
+    ## output buffer when done.
     do
     {
-        try
-        {
-            $read = $stream.Read($buffer, 0, 1024)
+        ## allow data to buffer for a bit
+        start-sleep -m 1000
 
-            if($read -gt 0)
+        ## read what data is available
+        $foundmore = $false
+        $stream.ReadTimeout = 1000
+
+        do
+        {
+            try 
             {
-                $foundmore = $true
-                $outputBuffer += ($encoding.GetString($buffer, 0, $read))
+                $read = $stream.Read($buffer, 0, 1024) 
+
+                if($read -gt 0) ## anything in the buffer?
+                {
+                    $foundmore = $true ## yes, there is
+                    $outputBuffer += ($encoding.GetString($buffer, 0, $read)) ## add string to output
+                    echo $outputBuffer ## added as a test to see how the buffer looks at each step
+                }
+            } 
+            catch 
+            { 
+                $foundMore = $false; $read = 0 ## couldn't read the buffer, exits the function 
             }
         } 
-        catch 
-        { 
-            $foundMore = $false; $read = 0 
-        }
-        } while($read -gt 0)
-        } while($foundmore)
+        while($read -gt 0) ## if there is something still in the buffer, loop back through
+    } 
+    while($foundmore) ## first time through, foundmore will be false so enter do
 
-        $outputBuffer
-    }
-
-
-    function Main
-    {
-      ## open the socket, and connect to the device on the specified port
-
-      write-host "Connecting to $hostname on port $port"
-      try
-      {
-          $socket = new-object System.Net.Sockets.TcpClient($hostname, $port)
-      }
-      catch 
-      {
-        $hostsWithError += $hostname
-    }
-    if ($stream)
-    {
-        write-host "Connected. Press ^D followed by [ENTER] to exit.`n"
-
-        $stream = $socket.GetStream()
-
-        $writer = new-object System.IO.StreamWriter $stream
-
-        ## receive the output that has buffered so far
-        $SCRIPT:output += GetOutput
-
-        $writer.WriteLine($username)
-        $writer.Flush()
-        Start-Sleep -m $commandDelay
-        $writer.WriteLine($password)
-        $writer.Flush()
-        Start-Sleep -m $commandDelay
-        $writer.WriteLine($termlength)
-        $writer.Flush()
-        Start-Sleep -m $commandDelay
-        $writer.WriteLine($enable)
-        $writer.Flush()
-        Start-Sleep -m $commandDelay
-        $writer.WriteLine($enablepassword)
-        $writer.Flush()
-        Start-Sleep -m $commandDelay
-        ## start executing our commands
-        foreach ($command in $runCommands)
-        {
-            echo "Executing $command"
-            $writer.WriteLine($command) ## executes commands from runCommands array
-            $writer.Flush()
-            Start-Sleep -m $commandDelay
-        }
-        $SCRIPT:output += GetOutput
-
-        ## Close the streams
-        $writer.Close()
-        $stream.Close()
-
-        try{
-            ## set our filename - NEED TO MOVE THIS TO THE SETTINGS AT TOP
-            $output | Out-File ("$filepath$hostname-$filename-$time$ext") 
-            echo "Config saved successfully"
-        }
-        catch{
-            echo "Error saving config"
-        }
-        
-
-    }
-    
+    $outputBuffer ## return the buffer output that we grabbed
 }
 
 
-## loop through each host/IP and get the running config
+function Main
+{
+    ## open the socket, and connect to the device on the specified port
+
+    write-host "$hostname : Connecting on port $port" 
+    $socket = new-object System.Net.Sockets.TcpClient ## create the socket
+
+    ## let's check if we can open the socket
+    try 
+    { 
+        $socket.Connect($hostname, $port)
+        Write-Host "$hostname : Connection successful"
+    }
+    catch 
+    { 
+        Write-Host "$hostname : Connection Failed"
+        $deadHosts += $hostname ## add the host to an array of dead hosts
+    }
+    finally
+    {
+        if ($socket.Connected) ## is our socket connected? let's login!
+        {
+            Write-Host "$hostname : Logging in"
+            ## get the data stream from the socket
+            $stream = $socket.GetStream() 
+            ## get ready write to the socket
+            $writer = new-object System.IO.StreamWriter $stream 
+
+            ## we'll assume that our connection is fine, we'll check soon
+
+            ## send the username
+            $writer.WriteLine($username)
+            $writer.Flush()
+            Start-Sleep -m $commandDelay
+            ## send the password
+            $writer.WriteLine($password)
+            $writer.Flush()
+            Start-Sleep -m $commandDelay
+            ## prevent the -- more -- when we run long commands
+            $writer.WriteLine($termlength)
+            $writer.Flush()
+            Start-Sleep -m $commandDelay
+            ## enter enable mode, this command is harmless if we are already
+            ## priv 15 and we can avoid checking for the enable prompt :)
+            ## if we aren't priv 15 then we will be prompted for the enable password. 
+            $writer.WriteLine($enable)
+            $writer.Flush()
+            Start-Sleep -m $commandDelay
+            ## send the enable password
+            $writer.WriteLine($enablepassword)
+            $writer.Flush()
+            Start-Sleep -m $commandDelay
+
+            ## all the output from the login should be in the buffer now, let's
+            ## grab it and see if we were able to login
+            $SCRIPT:output += GetOutput ## assign buffer content to a variable
+
+            ## check the variable for a privileged exec prompt $pattern is the
+            ## regex to find the enable prompt: Hostname#
+            ## $result should return True if we found our priv exec prompt
+            $result = $output -Match $pattern 
+
+            ## if we have a prompt then we can run some commands
+            if ($result)
+            {
+                Write-Host "$hostname : Login successful!"
+                ## clear the buffer because our login was successful and we don't
+                ## want to see the login process in our output file
+                $output = ''
+                ## start executing our commands that we set-up earlier
+                foreach ($command in $runCommands)
+                {
+                    echo "$hostname : Executing $command"
+                    $writer.WriteLine($command) ## executes commands from runCommands array
+                    $writer.Flush()
+                    Start-Sleep -m $commandDelay
+                }
+
+                ## get the output from the buffer
+                $SCRIPT:output += GetOutput
+
+                ## bit of exception handling, just in case the user made a mistake when they
+                ## set the file-save directory
+                try
+                {
+                    ## set our filename 
+                    ## TODO: This should be an option in the settings so the user can customise
+                    ## their file output
+                    $output | Out-File ("$filepath$hostname-$filename-$time$ext") 
+                    echo "$hostname : Config saved successfully"
+                    echo "-------------------------------------"
+                    $SCRIPT:output = ""
+                }
+
+                catch
+                {
+                    ## TODO: should probably add something here, maybe prompt for a directory
+                    ## that actually exists, or check earlier on if it's valid.
+                    echo "$hostname : Error saving config"
+                    $SCRIPT:output = ""
+                }
+            }
+
+            else
+            {
+                ## we couldn't login or didn't get a privileged exec prompt. add the host
+                ## to a separate list hosts that are alive, but with perhaps different user
+                ## and password.
+                Write-Host "Login failed for $hostname"
+                $hostNoLogin += $hostname
+            }
+            ## Close the streams
+            $writer.Close()
+            $stream.Close()
+        }
+    }
+}
+
+
+## First part of the script that actually runs, check to see if the user specified
+## ssh or telnet and run up the appropriate client to connect through
 switch ($connectionType)
 {
     ssh {
         $port = 22
-        foreach ($IP_add in $Devicelist)
+        foreach ($IP_add in $Devicelist) ## iterate through hosts CSV file
         {
-            $hostname = $IP_add.IPAddress; 
+            $hostname = $IP_add.IPAddress; ## assign the IP to a variable
+
+            ## Exception handling as a failed connection will kill the whole script
+            ## this is the behaviour of the SSH-Sessions module
             try{
                 $connectionStatus = New-SshSession $hostname -Username $username -Password "$password" -ErrorAction "SilentlyContinue" ## start session
             }
+            ## TODO: need to actually do something with this exception rather than just
+            ## log it
             Catch{
                 $ErrorMessage = $_.Exception.Message
                 $FailedItem = $_.Exception.ItemName
             }
+            ## TODO: need to bring this up to speed with the telnet section so that we
+            ## can use all the commands in the array and just be a bit more 'smart'
             $Results = Invoke-Sshcommand -InvokeOnAll -Command "$c1" | Out-File "$filepath$hostname-$filename-$time$ext" ## save results
             Remove-SshSession -computername $hostname ## close session
         }
@@ -214,30 +279,40 @@ switch ($connectionType)
         $port = 23
         foreach ($IP_add in $Devicelist)
         {
-            $hostname = $IP_add.IPAddress; 
-            . Main
+            $hostname = $IP_add.IPAddress; ## assign the IP to a variable
+            . Main ## run the Main function to create a socket and execute our commands
         }
     }
     default {
-        "Unknown connectionType of: $connectionType"
+        ## if the user doesn't specify telnet or ssh in the settings then they hit this section
+        "Unknown connectionType of: $connectionType" 
         "Please select either 'ssh' or 'telnet'"
     }
 
 }
-if ($hostsWithError)
+## if we have any hosts that were not reachable
+if ($deadHosts)
 {
-   $errorCount = $hostsWithError.count 
-   echo "$errorCount hosts could not be reached:"
-   echo $hostsWithError 
+    $errorCount = $deadHosts.count ## count how many we couldn't reach
+    echo "$errorCount hosts could not be reached:" ## tell the user
+    echo $deadHosts ## list the hosts so they can check it out
 }
-
-
+if ($hostNoLogin)
+{
+    $errorCount = $hostNoLogin.count ## count how many we couldn't reach
+    echo "Could not login to $errorCount hosts:" ## tell the user
+    echo $hostNoLogin ## list the hosts so they can check it out
+}
+## keeps the window open so the user can see if there were any problems
+## 
+Write-Host "Operation complete"
 Write-Host "Press any key to continue ..."
 
 $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # DON'T ALTER THE CODE ABOVE
+# NO USER TUNABLE SETTINGS
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
